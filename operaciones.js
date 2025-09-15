06e2741fd10dd3334f324739cc2e937db5d2c697
Amós Helí <amosheli2004@gmail.com>
2025-09-15 01:11:14 -0600

    Agregue las function para poder realizar operaciones basicas como sumar, restar, dividir, multiplicar, modulo y regresar pi completo

--- operaciones.js
+++ operaciones.js
@@ -1,1 +1,47 @@
+function sumar(numero1, numero2){
+
+    return numero1 + numero2;
+
+}
+
+function restar(numero1, numero2){
+
+    return numero1 - numero2;
+
+}
+
+function multiplicar(numero1, numero2){
+
+    return numero1 * numero2;
+
+}
+
+function dividir(numero1, numero2){
+
+    if(numero2 == 0) "No se puede dividir entre 0";
+
+    return numero1/numero2;
+
+}
+
+function modulo(numero1, numero2){
+
+     if(numero2 == 0) "No se puede calcular el modulo con divisor 0";
+
+    return numero1%numero2;
+
+}
+
+const pi = Math.pi;
+
+module.exports = {
+
+    sumar,
+    restar,
+    dividir,
+    multiplicar,
+    modulo,
+    pi
+
+};
 
