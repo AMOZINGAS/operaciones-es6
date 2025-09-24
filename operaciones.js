0e9501b1b55614e06f446f2398b967a12ee9bdae
OrlandoLeyvaFontes <orlando.leyva245512@potros.itson.edu.mx>
2025-09-23 18:37:14 -0700

    comit

--- operaciones.js
+++ operaciones.js
@@ -9,7 +9,10 @@ function restar(numero1, numero2){
     return numero1 - numero2;
 
 }
+function adicionar(a, b) {
+    return a + b;
+}
 
-function multiplicar(a, b) {
-    return a * b;
+function sustraer(a, b) {
+    return a - b;
 }
